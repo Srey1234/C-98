d5a5fdc57882ad154cd7e842a1030ecf1cf83435
Srey1234 <74773452+Srey1234@users.noreply.github.com>
2021-03-23 20:39:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ console.log (event);
 var Content=event.results[0][0].transcript;
 console.log(event);
 if(Content=="take my selfie")
-{
+{  
+    console.log("taking slefie ---")
+
     speak();
 }
 
@@ -31,13 +33,20 @@ function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking your selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance (speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+
+    setTimeout(function()
+
+    {
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
@@ -51,5 +60,22 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 
+function take_snapshot()
+
+{
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML ='<img id ="selfie_image" src="'+data_uri+'">';
+});
+}
+
+
+function save()
+
+{
+    link =document.getElementById("link");
+    image =document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
 
     
